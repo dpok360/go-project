66cdace5f4c30e8837f5a177e1fd9289242817c3
Deepak Suryavanshi <deepakmac@DEEPAKs-MacBook-Air.local>
2025-10-20 13:12:30 +0545

    project structure

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello world")
+	})
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
 }
